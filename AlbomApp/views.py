36c93a652f24dce26847629defde0059f8f9bc37
AlienHM <68409324+AlienHM@users.noreply.github.com>
2021-07-27 18:30:42 +0400

    youtube version

--- AlbomApp/views.py
+++ AlbomApp/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category, Photo
 # Create your views here.
 
 def gallery(request):
+    category = request.GET.get('category')
+    print(category)
+    if category is None:
+        photos = Photo.objects.all()
+
+    else:
+        photos = Photo.objects.filter(category__name=category)
+
+
     categories = Category.objects.all()
-    photos = Photo.objects.all()
     context = {'categories':categories, 'photos':photos}
     return render(request, 'photos/gallery.html', context)
 
@@ -15,4 +23,24 @@ def ViewPhoto(request, pk):
 
 
 def AddPhoto(request):
-    return render(request, 'photos/add.html')
+    categories = Category.objects.all()
+    if request.method == 'POST':
+        data = request.POST
+        image = request.FILES.get('image')
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+            category, created = Category.objects.get_or_create()
+        else:
+            category = None
+
+
+        photo = Photo.objects.create(
+            category = category,
+            description = data['description'],
+            image = image,
+        )
+        return redirect('gallery')
+    context = {'categories':categories}
+
+    return render(request, 'photos/add.html', context)
