28575faf71fc4dc099b595bab7c22e6b647bcaa3
AlienHM <68409324+AlienHM@users.noreply.github.com>
2021-08-07 23:41:13 +0400

    mkmn

--- Albom/settings.py
+++ Albom/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'AlbomApp.apps.AlbomappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'AlbomApp.apps.AlbomappConfig',
 ]
 
 MIDDLEWARE = [
